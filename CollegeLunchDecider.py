562219ef836a63b1e5eef1819dc90d3117a66ac8
Perhaps Kittens <kittens@basket>
2013-02-16 13:10:40 +0000

    Lunch script gets current system time.

--- CollegeLunchDecider.py
+++ CollegeLunchDecider.py
@@ -1,19 +1,31 @@
 import random
+from datetime import datetime
 
-n = random.randint(1,6)
+# Get the current time and hope that the user set their clock correctly.
+# h is current hour and m is current minute.
+h = datetime.now().hour
+m = datetime.now().minute
+
+# n is the (sudo)random number being generated and used for decisions.
+n = random.randint(1,7)
 
 print('Your computer has made the decision for you!')
 print('Now go to:')
 
+# Lazy options (doesn't require leaving campus, even for the sandwich shop).
 if (n==1):
-  print('Sandwich shop.')
+  print('Library Cafe')
 elif (n==2):
   print('JCR')
+# Meh options (fairly close to campus).
 elif (n==3):
-  print('Library Cafe')
+  print('Sandwich shop')
 elif (n==4):
   print('Chopstix')
 elif (n==5):
   print('Tesco')
 elif (n==6):
   print('Burger King')
+# Adventure options (Prepared to go a long way for food).
+elif (n==7):
+  print('McDonalds')
--- README.md
+++ README.md
@@ -2,3 +2,6 @@ Life Scripts
 ============
 
 Scripts that help in making everyday life decisions.
+
+College Lunch Decider
+---------------------
